3832194707b158e62220bb220ba452fc9ef848a1
jacky <jacky.yuan@dcmail.ca>
2020-11-17 21:23:16 -0500

    Add project files.

---
+++ NetdLab4-students4cheaptext/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetdLab4_students4cheaptext.Models;
+
+namespace NetdLab4_students4cheaptext.Controllers
+{
+    public class HomeController : Controller
+    {
+        //create a list of textbook objects
+        List<textbook> textBookList = new List<textbook>();
+
+        //when the index page is referenced
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //when the appraise page is referenced
+        [HttpGet]
+        public IActionResult Appraise()
+        {
+            return View();
+
+
+        }
+
+        //when the appraised page is referenced in post
+        [HttpPost]
+        public IActionResult Appraise(textbook model)
+        {
+            //if data entered in appraise page is valid
+            if (ModelState.IsValid)
+            {
+                ViewData["titleText"] = "Your textbook: " + model.textTitle + ", version:";
+                ViewData["valueText"] = model.textVersion + " was appraised at: $" + model.calcValue(model.textPrice, model.textCondition);
+
+                return View("Appraised", model);
+            }
+            //if invalid data was entered in the appraise page
+            else
+            {
+                return View("Bad");
+            }
+        }
+    }
+}
---
+++ NetdLab4-students4cheaptext/Models/textbook.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetdLab4_students4cheaptext.Models
+{
+    public class textbook
+    {
+        //creates getters and setters for each textbook attribute
+        public string textTitle { get; set; }
+
+        public Int64 textISBN { get; set; }
+
+        public double textVersion { get; set; }
+
+        public double textPrice { get; set; }
+
+        public string textCondition { get; set; }
+
+        //default constructor
+        public textbook()
+        {
+
+        }
+
+        //parameterized constructor
+        public textbook(string textTitle, int textISBN, double textVersion, double textPrice, string textCondition)
+        {
+            this.textTitle = textTitle;
+            this.textISBN = textISBN;
+            this.textVersion = textVersion;
+            this.textPrice = textPrice;
+            this.textCondition = textCondition;
+        }
+
+        //function for calculating the value of a textbook
+        public double calcValue(double textPrice, string textCondition)
+        {
+            //for like new condition
+            if (textCondition == "likeNew")
+            {
+                return textPrice/2;
+            }
+            //for good condition
+            else if (textCondition == "good") 
+            {
+                return textPrice/3;
+            }
+            //for bad condition
+            else 
+            {
+                return textPrice/4;
+            }
+        }
+
+    }
+
+}
